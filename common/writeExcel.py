376b0444c5c8c70022380ff9f18677f495056ad2
liuJinYang6 <504552075@qq.com>
2020-04-08 23:24:31 +0800

    将结果写入Excel文件

---
+++ common/writeExcel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2020-04-08 23:14
+# @Author : Go ku
+# @File : writeExcel.py
+
+import xlrd
+
+
+class WriteExcel(object):
+    """将测试结果写入excel"""
+
